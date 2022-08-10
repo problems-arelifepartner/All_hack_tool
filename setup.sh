318281a665c0be5ddee9391bb76db1530c4e2730
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2022-08-10 23:49:57 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#use it in your own risk
+#code : problems-arelifepartner
+apt update && apt upgrade -y
+apt install cmatrix -y
+termux-setup-storage -y
+gem install colorize
+echo 'cmatrix -Cred -L' >> ~/.bashrc
+clear
+echo '
+  m    m          mm            mmm         m    m
+ #    #          ##          m"   "        #  m"
+ #mmmm#         #  #         #             #m#
+ #    #         #mm#         #             #  #m
+ #    #        #    #         "mmm"        #   "m'
+echo "                          [code by <MR|D3V1L>]"
+sleep 3
+clear
+echo " I hope you like it"
+sleep 2
+echo " We are not responsible for any problems"
+sleep 2
+clear 
+ruby .uoykcuf.rb
+exit
